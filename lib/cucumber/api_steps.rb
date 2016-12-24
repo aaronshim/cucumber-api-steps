d986da685ed341cadfd27c48bbf0547af57788c8
Aaron Shim <aaron.k.shim@gmail.com>
2016-12-23 23:39:38 -0800

    extra sanitization

--- lib/cucumber/api_steps.rb
+++ lib/cucumber/api_steps.rb
@@ -44,7 +44,7 @@ When /^I send a (GET|PATCH|POST|PUT|DELETE) request (?:for|to) "([^\"]*)"(?: wit
     if input.class == Cucumber::MultilineArgument::DataTable
       request_json = input.rows_hash
     else
-      request_json = input
+      request_json = JSON.parse(input).to_json
     end
   end
   header 'Content-Type', 'application/json'
